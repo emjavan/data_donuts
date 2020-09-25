893d38dc04b7e1bb9f330efb30ed3ae28ed657d4
Emily Javan <emjavan@gmail.com>
2020-09-25 03:56:07 -0500

    added discrete maps

--- map_covid_cases.R
+++ map_covid_cases.R
@@ -1,12 +1,12 @@
 #### Data & Donuts Sept. 25, 2020 ####
 # By Emily Javan 
 # Note: ending a line with 4 '#' signs will create headers in document outline
-# The code below will walk you through plotting data from the NYT for COVID-19 cases in each US county.
+# The code below will walk you through plotting data from the NYT for COVID-19 cases/deaths in each US county.
 
 #### IMPORT LIBRARIES ####
-library(tidyverse)
-library(usmap) 
-library(ggplot2)
+library(tidyverse) # needed to do %>% pipes
+library(usmap) # where county polygons come from
+library(ggplot2) # how we plot the county level data
 
 #### GET DATA ####
 # NYT case data provides cumulative cases through time, but a county is only added once a case is detected
@@ -15,6 +15,7 @@ typeof(cty_case_data$fips) # Even though the fips code is a number it is coded a
 length(unique(cty_case_data$fips)) # On Aug. 31, 2020 I counted 3204 counties
 max_date = as.character(max(cty_case_data$date[!is.na(cty_case_data$date)])) # determine the most recent date in column
 cty_case_data$date = as.character(cty_case_data$date) # convert all dates from double to character
+
 
 # Census provided county population data up to 2019, but a lot of other stats as well
 cty_pop_data = read_csv("https://www2.census.gov/programs-surveys/popest/datasets/2010-2019/counties/totals/co-est2019-alldata.csv")
@@ -50,7 +51,7 @@ length(unique(all_counties_cases$fips)) # quick check we have as many counties a
 all_counties_cases$cases_per_100 = round(all_counties_cases$cases/all_counties_cases$pop19*100, 0)
 
 #### PLOT MAPS ####
-# Heat map of US cases at county level
+# Map of US cases at county level
 all_counties_cases %>%
   ggplot(aes(x = x, y = y)) +
   geom_polygon(aes(group = group, fill = cases), color = "black", size = 0.1) +
@@ -60,6 +61,7 @@ all_counties_cases %>%
         legend.title=element_text(size=8),
         legend.text=element_text(size=6))
 
+# cases per 100 people
 all_counties_cases %>%
   ggplot(aes(x = x, y = y)) +
   geom_polygon(aes(group = group, fill = cases_per_100), color = "black", size = 0.1) +
@@ -69,14 +71,40 @@ all_counties_cases %>%
         legend.title=element_text(size=8),
         legend.text=element_text(size=6))
 
-# Heat map of US cases at county level
+# cases discrete
+breaks = quantile(all_counties_cases$cases) # seperate the data into 4 quantiles/bins
+names(breaks) = NULL # remove names, so we only have numbers
+all_counties_cases$bin_cases = cut(all_counties_cases$cases, breaks=breaks, # cut relabels data with bin range
+                              include.lowest=TRUE, right=FALSE)
 all_counties_cases %>%
   ggplot(aes(x = x, y = y)) +
-  geom_polygon(aes(group = group, fill = deaths), color = "black", size = 0.1) +
-  scale_fill_gradient(low = "gainsboro", high = "dark red", name = paste0("Deaths ", max_date) )+
+  geom_polygon(aes(group = group, fill = bin_cases), color = "black", size = 0.1) +
+  scale_fill_manual(values = c("gainsboro", "darkgoldenrod1", "darkorange3", "indianred4"), 
+                    name = paste0("Cases \n", max_date) )+
   scale_x_continuous("", breaks = NULL) + scale_y_continuous("", breaks = NULL)+
   theme(panel.background = element_rect(color = "white", fill = "white"),
         legend.title=element_text(size=8),
         legend.text=element_text(size=6))
+
+# cases per 100 people discrete
+max(all_counties_cases$cases_per_100)
+bin_breaks = c(0, 1, 2, 8, 15) 
+labs = c("0", "1", "2", "8-15")
+all_counties_cases$bins = cut(all_counties_cases$cases_per_100, breaks=bin_breaks, 
+           include.lowest=TRUE, right=FALSE, labels = labs)
+all_counties_cases %>%
+  ggplot(aes(x = x, y = y)) +
+  geom_polygon(aes(group = group, fill = bins), color = "black", size = 0.1) +
+  scale_fill_manual(values = c("gainsboro", "darkgoldenrod1", "darkorange3", "indianred4"), 
+                    name = paste0("Cases per 100 \n", max_date) )+
+  scale_x_continuous("", breaks = NULL) + scale_y_continuous("", breaks = NULL)+
+  theme(panel.background = element_rect(color = "white", fill = "white"),
+        legend.title=element_text(size=8),
+        legend.text=element_text(size=6))
+
+#### AT HOME ####
+# Use the code above to make a discrete map of death data at county or state level
+
+
 
 
